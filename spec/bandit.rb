20394b172ab16f4c0790b5fd4a05f67f543c0782
Ben Doyle <doyle.ben@gmail.com>
2011-07-17 09:56:03 -0400

    remove test unit, in preference for rspec

---
+++ spec/bandit.rb
@@ -0,0 +1,7 @@
+require "spec_helper"
+
+describe Bandit::Bandit do
+  it "should do something" do
+    pending "write some code!"
+  end
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,2 @@
+require File.join(File.dirname(__FILE__), "..", "lib", "bandit.rb")
+require File.join(File.dirname(__FILE__), "..", "lib", "bandit", "bandit.rb")
